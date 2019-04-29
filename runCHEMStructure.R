4d7f6a2136b48807ddf4e4bf342af418408c94aa
Anthony Nash <anthony.nash@dpag.ox.ac.uk>
2019-04-29 13:37:08 +0100

    Add files via upload

---
+++ runCHEMStructure.R
@@ -0,0 +1,33 @@
+source("CHEMAPI.R")
+
+#I don't think I can call external to the server i.e., net access
+#drugFile <- loadCIDFile("drugs_names.txt")
+compounds <- loadSDFFile("compounds.sdf") 
+
+aromaticMatrix <- clusterWithFMCSAromatic(compounds)
+saveClusterMatrix(aromaticMatrix,"aromaticMatrix.txt")
+
+ringMatrix <- clusterWithFMCSRing(compounds)
+saveClusterMatrix(ringMatrix,"ringMatrix.txt")
+
+staticMatrix <- clusterWithFMCSStatic(compounds)
+saveClusterMatrix(staticMatrix,"staticMatrix.txt")
+
+stop("STOPPING")
+
+print("Loading CID entries.")
+cidDF <- loadCHEMDF("cidDF.rda")
+cidVector <- cidDF$CID
+print(paste("A total of",length(cidVector),"CID entries."))
+
+db <- getBioassayDatabase("pubchem_protein_only.sqlite")
+print("Building bioassay cluster matrix.")
+clusterMatrix <- clusterCompoundsByActivityProfile(db, as.character(cidVector))
+
+#print("Saving bioassay cluster matrix.")
+#df <- as.data.frame(clusterMatrix)
+#saveCHEMDF(df,"clusterMatrix.rda")
+#saveClusterMatrix(clusterMatrix,"bioactivityCluster.txt")
+
+
+disconnectBioassayDB(db)
